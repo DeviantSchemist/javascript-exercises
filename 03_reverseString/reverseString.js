b7caa8300aebca107c6e81671130416ec9c9a1c7
Michael Ngo <michaelngo1337@yahoo.com>
2022-07-14 14:19:13 -0700

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(myString) {
+  if (myString === '') {
+    return '';
+  }
 
+  return myString.split('').reverse().join('');
 };
 
 // Do not edit below this line
