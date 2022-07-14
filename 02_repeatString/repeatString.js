63faeb1016a45df0fa9d1a7636904b55edbac29f
Michael Ngo <michaelngo1337@yahoo.com>
2022-07-14 00:23:26 -0700

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(myString, myNum) {
+  if (myString === '') {
+    return '';
+  }
+  if (myNum < 0) {
+    return 'ERROR';
+  }
 
+  let result = '';
+
+  for (let i = 1; i <= myNum; i++) {
+    result += myString;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
