9ba00ec693acccd74a8707de3cacb4c0d068b25a
Michael Ngo <michaelngo1337@yahoo.com>
2022-07-14 21:11:27 -0700

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof(num1) !== 'number' || typeof(num2) !== 'number' || num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
 
+  const largerNumber = num1 > num2 ? num1 : num2;
+  const smallerNumber = num1 < num2 ? num1 : num2;
+
+  let total = smallerNumber;
+
+  for (let i = smallerNumber+1; i <= largerNumber; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
