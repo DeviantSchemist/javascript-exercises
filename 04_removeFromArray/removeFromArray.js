20c9d287e1e54c27b1f5cad87f472daaebd6c5b0
Michael Ngo <michaelngo1337@yahoo.com>
2022-07-14 19:59:45 -0700

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...thingsToRemove) {
 
+  for (let i = 0; i < thingsToRemove.length; i++) {
+    if (myArray.includes(thingsToRemove[i])) {
+      myArray.splice(myArray.indexOf(thingsToRemove[i]), 1);
+    }
+  }
+  
+  return myArray;
 };
 
 // Do not edit below this line
