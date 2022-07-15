035aa815f3237d8cef33d8292cec0dc945f22413
Michael Ngo <michaelngo1337@yahoo.com>
2022-07-15 15:53:40 -0700

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  const celsius = (num - 32) * (5/9);
+  return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  const farenheit = (num * (9/5)) + 32;
+  return Math.round(farenheit * 10) / 10;
 };
 
 // Do not edit below this line
