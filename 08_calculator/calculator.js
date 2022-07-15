ab0cc556e2b3f6c9dcfa3dd36b2168dfcd06e851
Michael Ngo <michaelngo1337@yahoo.com>
2022-07-15 16:43:51 -0700

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	if (!myArray.length) {
+    return 0;
+  }
+
+  if (myArray.length === 1) {
+    return myArray[0];
+  }
+
+  return myArray.reduce((total, currentValue) => total + currentValue);
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  return myArray.reduce((total, currentValue) => total * currentValue);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let answer = 1;
+
+  for (let i = 0; i < power; i++) {
+    answer *= base;
+  }
+
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
