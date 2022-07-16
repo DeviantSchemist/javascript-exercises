4cb0ad981a7688b6f8e5dcb4344fa91033615666
Michael Ngo <michaelngo1337@yahoo.com>
2022-07-15 18:23:47 -0700

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  const loop = Number(num);
 
+  if (loop < 0) {
+    return 'OOPS';
+  }
+
+  let a = 0, b = 1, c = loop;
+
+  for (let i = 2; i <= loop; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return c;
 };
 
 // Do not edit below this line
