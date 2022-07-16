56738ca9113f574c6eb066572dc27975be9b1797
Michael Ngo <michaelngo1337@yahoo.com>
2022-07-15 17:50:56 -0700

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  const sanitizedInput = word.replace(regex, '');
+  const cleanString = sanitizedInput.replace(/\s+/g, '');
+  const reversed = cleanString.toLowerCase().split('').reverse().join('');
+  const lowerCase = cleanString.toLowerCase();
+  return lowerCase === reversed;
 };
 
 // Do not edit below this line
